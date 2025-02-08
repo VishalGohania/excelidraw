012ca7d30e8c166b7217b69176cefe49fb71ca87
VishalGohania <140372374+VishalGohania@users.noreply.github.com>
2025-02-08 18:41:42 +0530

    Completed http layer

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,100 @@
-import express from 'express';
+import express from "express";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types";
+import { middleware } from "./middleware";
+import jwt from "jsonwebtoken";
+import { prismaClient } from "@repo/db/client"
+
 
 const app = express();
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+
+  const parsedData = CreateUserSchema.safeParse(req.body);
+  if(!parsedData.success) {
+    console.log(parsedData.error)
+    res.json({
+      message: "Incorrect Inputs"
+    })
+    return;
+  }
+  //db call
+  try {
+    const user = await prismaClient.user.create({
+    data: {
+      email: parsedData.data?.username,
+      password: parsedData.data.password,
+      name: parsedData.data.name
+    }   
+  })
+  res.json({
+    userId: user.id
+  })
+} catch(e) {
+  res.status(411).json({
+    message: "User already exists with this username"
+  })
+}
+  
+})
+
+app.post("/signin", async (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+  if(!parsedData.success) {
+    res.json({
+      message: "Incorrect Inputs"
+    })
+    return;
+  }
+  //db call
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: parsedData.data?.username,
+        password: parsedData.data?.password
+      }     
+    })
+   if(!user) {
+    res.status(403).json({
+      message: "Not authorised"
+    })
+    return;
+  }
+
+  const token = jwt.sign({
+    userId: user?.id
+  }, JWT_SECRET)
+
+  res.json({
+    token
+  })
+})
+
+app.post("/room", middleware, async (req, res) => {
+  const parsedData = CreateRoomSchema.safeParse(req.body);
+  if(!parsedData.success) {
+    res.json({
+      message: "Incorrect Inputs"
+    })
+    return;
+  }
+  const userId = req.userId;
+  //db
+  try {
+    const room = await prismaClient.room.create({
+      data: {
+        slug: parsedData.data.name,
+        adminId: userId!
+      }
+    })
+    res.json({
+      roomId: room.id
+    })
+  } catch(e) {
+    res.status(411).json({
+      message: "Room already exists with this name"
+    })
+  }
+})
 
 app.listen(3001);
