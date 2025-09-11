249b160110932239bbb9527111105fac92e27ed2
VishalGohania <vishalgohania99@gmail.com>
2025-09-11 11:14:55 +0530

    added production http and ws urls

--- apps/excelidraw-frontend/config.ts
+++ apps/excelidraw-frontend/config.ts
@@ -1,8 +1,20 @@
 
 const getBackendUrl = () => {
+  if (process.env.NEXT_PUBLIC_HTTP_BACKEND) {
+    return process.env.NEXT_PUBLIC_HTTP_BACKEND;
+  }
+
   const port = process.env.HTTP_PORT || '3001';
   return `http://localhost:${port}`;
 }
 
+const getWebSocketUrl = () => {
+  if (process.env.NEXT_PUBLIC_WS_URL) {
+    return process.env.NEXT_PUBLIC_WS_URL;
+  }
+
+  return "ws://localhost:8080";
+}
+
 export const HTTP_BACKEND = getBackendUrl();
-export const WS_URL = "ws://localhost:8080";
+export const WS_URL = getWebSocketUrl();
